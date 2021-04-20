813f82b8e263de58d5d6d040d9bb14ec7fa6f284
RiyadTangil <riyadkhanxp2@gmail.com>
2021-04-20 09:19:50 +0600

    delete btn added

--- index.js
+++ index.js
@@ -132,10 +132,12 @@ client.connect(err => {
             res.send(result.modifiedCount > 0)
           })
       })
-
-
-
-
+      app.delete('/delete/:id', (req, res) => {
+        serviceCollection.deleteOne({ _id: ObjectId(req.params.id) })
+            .then(result => {
+              res.send(result.deletedCount > 0);
+            })
+        })
 });
 
 
@@ -145,74 +147,3 @@ app.listen(process.env.PORT || port)
 
 
 
-
-
-
-// app.post('/addAppointment', (req, res) => {
-//     const appointment = req.body;
-//     appointmentCollection.insertOne(appointment)
-//         .then(result => {
-//             res.send(result.insertedCount > 0)
-//         })
-// });
-
-// app.get('/appointments', (req, res) => {
-//     appointmentCollection.find({})
-//         .toArray((err, documents) => {
-//             res.send(documents);
-//         })
-// })
-
-// app.post('/appointmentsByDate', (req, res) => {
-//     const date = req.body;
-//     const email = req.body.email;
-//     doctorCollection.find({ email: email })
-//         .toArray((err, doctors) => {
-//             const filter = { date: date.date }
-
-//             if (doctors.length === 0) {
-//                 filter.email = email;
-//             }
-
-//             appointmentCollection.find(filter)
-//                 .toArray((err, documents) => {
-//                     console.log(email, date.date, doctors, documents)
-//                     res.send(documents);
-//                 })
-//         })
-// })
-
-// app.post('/addADoctor', (req, res) => {
-//     const file = req.files.file;
-//     const name = req.body.name;
-//     const email = req.body.email;
-//     const newImg = file.data;
-//     const encImg = newImg.toString('base64');
-
-
-//     var image = {
-//         contentType: file.mimetype,
-//         size: file.size,
-//         img: Buffer.from(encImg, 'base64')
-//     };
-
-//     doctorCollection.insertOne({ name, email, image })
-//         .then(result => {
-//             res.send(result.insertedCount > 0);
-//         })
-// })
-
-// app.get('/doctors', (req, res) => {
-//     doctorCollection.find({})
-//         .toArray((err, documents) => {
-//             res.send(documents);
-//         })
-// });
-
-// app.post('/isDoctor', (req, res) => {
-//     const email = req.body.email;
-//     doctorCollection.find({ email: email })
-//         .toArray((err, doctors) => {
-//             res.send(doctors.length > 0);
-//         })
-// })
